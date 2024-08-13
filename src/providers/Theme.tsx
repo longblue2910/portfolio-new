1447fa2ac9ba29268f697069b867a15a832379d3
Rim da silva <tienlong291099@gmail.com>
2024-08-13 07:50:03 +0700

    first commit

---
+++ src/providers/Theme.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
